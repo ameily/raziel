3574adf687a2a3998d2b266b528d0be2853d3bd4
Adam Meily <meily.adam@gmail.com>
2015-07-16 00:45:17 -0400

    moved explorer to its own routes file

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/explorer', function(req, res, next) {
-  res.render('explorer');
-});
-
 module.exports = router;
