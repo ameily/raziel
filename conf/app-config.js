6ab9f25b717d83970a7b3cc9ad5fc51c708aff56
Adam Meily <meily.adam@gmail.com>
2015-09-02 00:09:36 -0400

    initial commit of configuration

---
+++ conf/app-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  ///
+  /// Web server port.
+  ///
+  port: 8080,
+
+  ///
+  /// MongoDB configuration.
+  ///
+  mongodb: "mongodb://localhost/raziel",
+
+  ///
+  /// Root storage location to hold file content. Relative paths will be
+  /// relative to the base raziel directory.
+  ///
+  storage: "./uploads"
+};
